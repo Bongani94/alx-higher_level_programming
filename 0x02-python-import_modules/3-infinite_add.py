38a7bf0f97597a1a8e305dd6ee39b51afdaac331
bongani <bonganibaloyi94@gmail.com>
2022-12-01 15:57:45 +0000

    python

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     index = 1
     res = 0
     while index <= argv_count:
-	res += int(sys.argv[index])
-	index += 1
+        res += int(sys.argv[index])
+        index += 1
     print("{:d}".format(res))
