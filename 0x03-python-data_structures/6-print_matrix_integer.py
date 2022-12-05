89dfdb331bf34672173ec972b78e93bb124db143
Bongani <bonganibaloyi94@gmail.com>
2022-12-05 07:44:59 -0800

    task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in matrix:
-        i = 1
-    for j in i:
-        if i == len(i):
-            print("{:d}".format(j), end=" ")
-    else:
-        print("{:d}".format(j), end=" ")
-    i = i + 1
-    print()
+    for row in matrix:
+        for col in row:
+            print("{:d}".format(col). end=" " if col != row[-1] else "")
+        print()
