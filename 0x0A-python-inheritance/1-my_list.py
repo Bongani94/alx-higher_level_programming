6195c06787c589fb5d9549e281be55ea0ca81849
Bongani <bonganibaloyi94@gmail.com>
2023-01-09 11:31:45 -0800

    inheritance

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-"""Inherits from list"""
+"""
+Contains class MyList
+inherits from list; has public instance method to print sorted
+"""
 
 
 class MyList(list):
-    """inherits from list"""
-
+    """inherits from list
+    methods:
+    print_sorted(self)
+    """
     def print_sorted(self):
-        """print sorted list"""
+        """prints list of ints all sorted in ascending order"""
         print(sorted(self))
--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-"""check if object if instance of a class that
-inherited from specified class"""
+"""
+Contains method inherits_from
+returns True if obj is instance of class that it inherits from or is subcls of
+"""
 
 
 def inherits_from(obj, a_class):
-    """return True if object is an instance of a class that
-    inherited from specified class"""
-    if issubclass(obj.__class__, a_class) is True:
-        if obj.__class__ is not a_class:
-            return True
-    else:
-        return False
+    """
+    Notes:
+        use type() to get specific class
+        use isinstance() to get class and any parent classes too
+        use issubclass() to get what object is a subclass of
+    Return:
+        True if obj is instance of class that it inherits from or is subcls of
+    """
+    return (type(obj) is not a_class and issubclass(type(obj), a_class))
