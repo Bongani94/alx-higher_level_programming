95bff2b8eeb838e6dc2360e79a0c796a912c2fd8
Bongani Baloyi <bonganibaloyi94@gmail.com>
2023-03-31 14:41:56 -0700

    network

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,12 +1,26 @@
 #!/usr/bin/python3
-"""Python script that takes in a URLand an email, sends a POST
-request to the passed URL with the email as a parameter, and
-displays the body of the response (decoded in utf-8)"""
+"""
+script that takes in a URL and an email, sends a POST request to the passed
+URL with the email as a parameter, and displays the body of the response
+(decoded in utf-8).
+"""
+import urllib.request
+import urllib.parse
+import sys
 
-from urllib import request, parse
-from sys import argv
 
 if __name__ == "__main__":
-    data = parse.urlencode({"email": argv[2]}).encode('utf-8')
-    with request.urlopen(argv[1], data) as page:
-        print(page.read().decode('utf-8'))
+    url = sys.argv[1]
+    email = sys.argv[2]
+    param = {
+        "email": email
+    }
+    query_string = urllib.parse.urlencode(param)
+    data = query_string.encode("ascii")
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        # If you do not pass the data argument, urllib uses a GET request.
+        # One way in which GET and POST requests differ is that POST requests
+        # often have "side-effects".
+        response_text = response.read().decode("utf-8")
+        print(response_text)
