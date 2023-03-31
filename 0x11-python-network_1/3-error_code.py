627e4f3ea6d10b8cfbd68793b21a010135a4d841
Bongani Baloyi <bonganibaloyi94@gmail.com>
2023-03-31 14:32:35 -0700

    network 1

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL, sends a request to the URL
-and displays the body of the response (decoded in utf-8)."""
-
-from urllib import request, error
+"""Sends a request to the URL and displays the body
+of the response decoded in utf8"""
+import urllib.request
+import urllib.error
 from sys import argv
-
 if __name__ == "__main__":
-        try:
-            with request.urlopen(argv[1]) as page:
-                print(page.read().decode('utf-8'))
-        except error.HTTPError as e:
-            print("Error code: {}".format(e.code))
+    try:
+        with urllib.request.urlopen(argv[1]) as response:
+            page = response.read()
+            print("{}".format(page.decode('utf8')))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
